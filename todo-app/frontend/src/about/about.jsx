a439df45bf656dd07216e942d4e6e363cee4071b
Guilherme Lopes <guilhermelopes@precisaosistemas.com.br>
2019-02-27 14:37:33 -0300

    todo-app

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Os cara é troll!</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Num gosta de doceeeeeeee..</p>
+    </div>
+)
